da870ab079e01f74cf53b79bf9454f8f36c8158f
Rene Dohmen <acidjunk@Mac.fritz.box>
2026-03-21 21:03:17 +0100

    Configure for GitHub Pages - update base path and links

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://virge-creator.github.io',
+  base: '/spinnen-posters',
+});
